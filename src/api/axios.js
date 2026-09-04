9e81813a2d709a1f5da5845b7ff8d1c5956d1112
Nnenna.Ezidiegwu <ezidiegwunnennafavour@gmail.com>
2026-09-04 16:12:36 +0100

    Updated the base url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://bill-payment-system.onrender.com/api/v1',
+  baseURL: 'http://16.171.43.59:8080/api/v1',
   //   baseURL: 'http://localhost:8080/api/v1',
   headers: {
     'Content-Type': 'application/json',
