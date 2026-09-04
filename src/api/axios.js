e49c545111632cd80c888e2e0bdc76cd6016cd4e
Nnenna.Ezidiegwu <ezidiegwunnennafavour@gmail.com>
2026-09-04 18:02:13 +0100

    Updated base url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://duckdns.org',
+  baseURL: 'https://payease.duckdns.org',
   //   baseURL: 'http://localhost:8080/api/v1',
   headers: {
     'Content-Type': 'application/json',
